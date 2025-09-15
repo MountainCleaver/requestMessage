7c57df9b3bfc1f5016cb974075d94b86aede8bd6
MountainCleaver <juliusromero009988@gmail.com>
2025-09-15 12:39:22 +0800

    added to do list

--- scenes/menu/scripts/boot_warning.gd
+++ scenes/menu/scripts/boot_warning.gd
@@ -1,5 +1,10 @@
 extends Control
+@onready var button: Button = $proceed/Button
+
+func _ready() -> void:
+	#TODO -> CHECK IF PLAYER LOGGED IN OR NOT. IF YES, THEN THE FUNCTION B ELOW SHOULD TAKE PLAYER TO MAIN MENU AND NOT TO LOGIN/SIGNUP PAGE
+	pass;
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("accept"):
+	if Input.is_action_pressed("accept") or button.button_pressed:
 		SignalBus.next_scene.emit("res://scenes/menu/menu_create_acc_bday_gender.tscn");
