a5e768c785f5d48ee0813d7b784f575fa8d67073
MountainCleaver <juliusromero009988@gmail.com>
2025-09-15 12:43:46 +0800

    will play animation and handle exit

---
+++ scenes/menu/scripts/menu_credits.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var button: Button = $back_tips/Panel/Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.play("scrolling");
+	animation_player.animation_finished.connect(_exit_credits);
+	button.pressed.connect(_exit_credits);
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		_exit_credits();
+
+
+func _exit_credits() -> void:
+	SignalBus.exit_overlay.emit();
+	queue_free();
