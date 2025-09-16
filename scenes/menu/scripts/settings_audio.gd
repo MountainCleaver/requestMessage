48d92b341db328a593b37b10de91020a38170da6
MountainCleaver <juliusromero009988@gmail.com>
2025-09-16 18:09:45 +0800

    save settings to resource

--- scenes/menu/scripts/settings_audio.gd
+++ scenes/menu/scripts/settings_audio.gd
@@ -7,11 +7,32 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	music_slider.value = Settings.music_volume;
-	sfx_slider.value = Settings.sfx_volume;
-	mute_check.button_pressed = Settings.mute_all_sounds;
+	music_slider.value = Settings.settings.music_volume;
+	sfx_slider.value = Settings.settings.sfx_volume;
+	mute_check.button_pressed = Settings.settings.mute_all_sounds;
+	
+	music_slider.value_changed.connect(_on_music_slider_value_changed);
+	sfx_slider.value_changed.connect(_on_sfx_slider_value_changed);
+	mute_check.toggled.connect(_mute_toggled);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _mute_toggled(toggled: bool):
+	if toggled:
+		Settings.settings.mute_all_sounds = true;
+		Settings.save_settings();
+	else:
+		Settings.settings.mute_all_sounds = false;
+		Settings.save_settings();
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	Settings.settings.music_volume = value;
+	Settings.save_settings();
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	Settings.settings.sfx_volume = value;
+	Settings.save_settings();
--- scenes/menu/scripts/settings_display.gd
+++ scenes/menu/scripts/settings_display.gd
@@ -6,18 +6,31 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var window_mode : String = Settings.window_mode;
+	var window_mode : String = Settings.settings.window_mode;
 	
-	brightness_slider.value = Settings.brightness;
+	brightness_slider.value = Settings.settings.brightness;
 	
 	if window_mode == "fullscreen":
 		radio_fullscreen.button_pressed = true;
 	elif window_mode == "windowed":
 		radio_winowed.button_pressed = true;
 		
+	radio_fullscreen.toggled.connect(_on_radio_fullscreen_toggled);
+	radio_winowed.toggled.connect(_on_radio_windowed_toggled);
+	brightness_slider.value_changed.connect(_on_brightness_slider_value_changed);
 
+func _on_radio_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		Settings.settings.window_mode = "fullscreen"
+		Settings.save_settings();
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print("full screen: " + str(radio_fullscreen.button_pressed));
-	print("windowed: "  + str(radio_winowed.button_pressed));
+func _on_radio_windowed_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		Settings.settings.window_mode = "windowed"
+		Settings.save_settings();
+
+func _on_brightness_slider_value_changed(value: float) -> void:
+	Settings.settings.brightness = value;
+	Settings.save_settings();
