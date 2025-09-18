a6b012f75ada5fc1380c2961fa32bd7bfb089d95
MountainCleaver <juliusromero009988@gmail.com>
2025-09-18 20:48:38 +0800

    change node refernce on button login

--- scenes/menu/scripts/acc_login_username_password.gd
+++ scenes/menu/scripts/acc_login_username_password.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready var signup_link: Button = $holder/login_link
+@onready var btn_login: Button = $holder/btn_login
 
 var api : String = "http://192.168.100.57/chat-app/apis/auth/login.php"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	btn_login.pressed.connect(_goto_main_menu);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +18,7 @@ func _process(delta: float) -> void:
 
 func _on_signup_link_pressed() -> void:
 	SignalBus.next_scene.emit("res://scenes/menu/menu_create_acc_creds.tscn");
+
+func _goto_main_menu(): #temporary fn
+	SignalBus.next_scene.emit("res://scenes/menu/menu_main.tscn")
+	
