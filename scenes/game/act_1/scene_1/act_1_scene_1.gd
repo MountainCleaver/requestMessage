74c057342beeb772669af9f8447668d87a8f51f2
MountainCleaver <juliusromero009988@gmail.com>
2025-09-19 13:36:59 +0800

    play camera intro pan

--- scenes/game/act_1/scene_1/act_1_scene_1.gd
+++ scenes/game/act_1/scene_1/act_1_scene_1.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @onready var storm_animation: AnimationPlayer = $storm_animation
+@onready var camera_animation: AnimationPlayer = $camera_animation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	camera_animation.play("intro_pan");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
