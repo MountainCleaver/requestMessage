6248ed68f644eae7bd5638880e6954304d0c313b
MountainCleaver <juliusromero009988@gmail.com>
2025-09-24 17:08:10 +0800

    add game progress data resource

---
+++ save_game_resource.gd
@@ -0,0 +1,16 @@
+extends Resource
+class_name SaveGameResource
+
+@export var current_scene : String = ""
+@export var current_act : String = ""
+@export var finished_scenes : Dictionary = {
+	"act_1": [],
+	"act_2": [],
+	"act_3": [],
+	"act_4": [],
+	"act_5": [],
+	"act_6": []
+}
+@export var restless : int = 0
+@export var relief : int = 0
+@export var given_lola_ising_flashlight : bool = false
