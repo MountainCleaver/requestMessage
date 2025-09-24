b4d864e343d8b9c6a64f2873bda4d82efe079931
MountainCleaver <juliusromero009988@gmail.com>
2025-09-24 23:23:22 +0800

    add clarification comment

--- scenes/game/act_1/scene_1/act_1_scene_1.gd
+++ scenes/game/act_1/scene_1/act_1_scene_1.gd
@@ -114,7 +114,7 @@ func _go_to_sleep() -> void:
 	await get_tree().create_timer(5).timeout;
 	
 	SaveManager.game_save.current_act = "act_1"
-	SaveManager.game_save.current_scene = "scene_2"
+	SaveManager.game_save.current_scene = "scene_2" # badly named I admit. this is for the 'continue' part in main menu
 	SignalBus.act_num_scene_num_done.emit("act_1", "scene_1", "res://scenes/game/act_1/scene_2/act_1_scene_2.tscn") # caught in save manager
 
 	print("act 1 scene 1 is done")
