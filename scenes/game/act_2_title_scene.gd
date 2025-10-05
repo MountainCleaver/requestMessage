82d3e7377fc87eea125fee1087c87fe1308fb7d7
Zenayuki <cancino.gerald138@gmail.com>
2025-10-05 12:21:05 +0800

    add narration to act 2 title scene

--- scenes/game/act_2_title_scene.gd
+++ scenes/game/act_2_title_scene.gd
@@ -3,6 +3,19 @@ extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	animation_player.play("in_animation");
-	await animation_player.animation_finished;
-	SignalBus.next_scene.emit("res://scenes/game/act_2/scene_1/act_2_scene_1.tscn"); 
+	# Fade-in title scene
+	animation_player.play("in_animation")
+	await animation_player.animation_finished
+	
+	# All 3 lines stacked vertically
+	var lines = [
+		"Some messages are not meant to be ignored.",
+		"…Some memories are best left forgotten.",
+		"…But they find you anyway."
+	]
+	
+	await NarrationPanel.show_narration_typewriter(lines, 0.05)
+	await NarrationPanel.hide_narration()
+	
+	# Fade transition after click
+	SignalBus.next_scene.emit("res://scenes/game/act_2/scene_1/act_2_scene_1.tscn")
