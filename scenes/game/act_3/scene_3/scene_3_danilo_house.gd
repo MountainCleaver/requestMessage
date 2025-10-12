06fd1f897ff28e7ce705e1a957ff0e13c11860b0
MountainCleaver <juliusromero009988@gmail.com>
2025-10-13 02:36:40 +0800

    add scene 3 specific signal script

---
+++ scenes/game/act_3/scene_3/scene_3_danilo_house.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	SignalBus.in_npc.emit("door")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	SignalBus.out_npc.emit("door")
