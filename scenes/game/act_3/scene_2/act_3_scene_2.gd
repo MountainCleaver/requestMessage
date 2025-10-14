34e4bdf3f404d48db7c151c2e920bc35cc8ade7d
MountainCleaver <juliusromero009988@gmail.com>
2025-10-14 14:03:18 +0800

    fix lock button, disabled at the begining

--- scenes/game/act_3/scene_2/act_3_scene_2.gd
+++ scenes/game/act_3/scene_2/act_3_scene_2.gd
@@ -63,7 +63,7 @@ func _ready() -> void:
 	
 	SignalBus.unknown_sender_unlocked = true
 	SignalBus.unknown_sender_label_visible = false
-	Hud.get_node("Control/phone/MarginContainer/lock_screen/Panel/lock").disabled = true
+	
 	danilo_collision_shape_2d.disabled = true
 	player_danilo.animation_locked = true
 	player_danilo.force_cannot_move = true
@@ -73,7 +73,9 @@ func _ready() -> void:
 
 	Hud.reset_phone_state()
 	await get_tree().create_timer(1.0).timeout
-
+	
+	Hud.get_node("Control/phone/MarginContainer/lock_screen/Panel/lock").disabled = true
+	
 	add_notification(CHAT_ICON, "Chat", "2 missed calls from Mira")
 	await get_tree().create_timer(1.0).timeout
 	add_notification(CHAT_ICON, "Chat", "Wendy: Dan, you didn’t even inform me you were going home. We were worried, hindi ka sumasagot sa chats ko. Akala ko may nangyari na sayo")
