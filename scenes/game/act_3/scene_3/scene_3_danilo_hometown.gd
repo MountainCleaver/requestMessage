f6f6e918338c803dfd19710d5d61afcfa982bafd
MountainCleaver <juliusromero009988@gmail.com>
2025-10-15 00:31:11 +0800

    update support scripts and scenes for act 3 and scene 3

--- scenes/game/act_3/scene_3/scene_3_danilo_hometown.gd
+++ scenes/game/act_3/scene_3/scene_3_danilo_hometown.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var first_time_in: bool = true
+
 func _on_tricycle_area_body_entered(body: Node2D) -> void:
 	if body.name == "player_danilo":
 		SignalBus.in_npc.emit("tricycle_hometown")
@@ -14,7 +16,19 @@ func _on_karatula_area_body_entered(body: Node2D) -> void:
 	if body.name == "player_danilo":
 		SignalBus.in_npc.emit("karatula")
 
-
 func _on_karatula_area_body_exited(body: Node2D) -> void:
 	if body.name == "player_danilo":
 		SignalBus.out_npc.emit("karatula")
+
+
+func _on_house_door_outside_body_entered(body: Node2D) -> void:
+	if first_time_in:
+		first_time_in = false
+		return
+	if body.name == "player_danilo":
+		SignalBus.in_npc.emit("door_outside")
+
+
+func _on_house_door_outside_body_exited(body: Node2D) -> void:
+	if body.name == "player_danilo":
+		SignalBus.out_npc.emit("door_outside")
--- scenes/game/act_3/scene_3/scene_3_town_center.gd
+++ scenes/game/act_3/scene_3/scene_3_town_center.gd
@@ -7,3 +7,11 @@ func _on_drugstore_door_body_entered(body: Node2D) -> void:
 func _on_drugstore_door_body_exited(body: Node2D) -> void:
 	if body.name == "player_danilo":
 		SignalBus.out_npc.emit("drugstore_door_outside")
+
+func _on_tricycle_area_body_entered(body: Node2D) -> void:
+	if body.name == "player_danilo":
+		SignalBus.in_npc.emit("tricycle_town_center")
+
+func _on_tricycle_area_body_exited(body: Node2D) -> void:
+	if body.name == "player_danilo":
+		SignalBus.out_npc.emit("tricycle_town_center")
