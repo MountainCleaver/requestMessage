63abac5a97978cdfc7ff7b77d9e31d0fb97730f1
Gerald <cancino.gerald138@gmail.com>
2025-10-24 01:07:56 +0800

    fix guide panel still showing up in scenes other than a4s1

--- globals/explorer_hud.gd
+++ globals/explorer_hud.gd
@@ -53,6 +53,7 @@ func _check_tutorial_guides():
 	var act = FlashlightManager.data.get("current_act", "")
 	var scene = FlashlightManager.data.get("current_scene", "")
 
+	# Only show tutorial in Act 4 Scene 1
 	if act == "act_4" and scene == "scene_1":
 		tutorial_active = true
 		_fade_in_guide(map_guide)
@@ -61,11 +62,17 @@ func _check_tutorial_guides():
 			_fade_in_guide(flashlight_guide)
 
 		var timer = Timer.new()
-		timer.wait_time = 7.0
+		timer.wait_time = 5.0
 		timer.one_shot = true
 		timer.autostart = true
 		add_child(timer)
 		timer.timeout.connect(Callable(self, "_end_tutorial_guides"))
+	else:
+		# Hide guides when not in Act 4 Scene 1
+		tutorial_active = false
+		_fade_out_guide(map_guide)
+		_fade_out_guide(flashlight_guide)
+
 
 func _end_tutorial_guides():
 	tutorial_active = false
@@ -127,6 +134,9 @@ func _on_hover_exit():
 # SCENE CHANGED
 # ===================
 func _on_scene_changed(act: String, scene: String):
+	_fade_out_guide(map_guide)
+	_fade_out_guide(flashlight_guide)
+
 	_update_flashlight_visibility()
 	_check_tutorial_guides()
 
