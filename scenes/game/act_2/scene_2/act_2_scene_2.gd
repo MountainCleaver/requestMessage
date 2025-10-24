5e38622c226efa6eb45e88b7c114b8d880a03518
Gerald <cancino.gerald138@gmail.com>
2025-10-24 22:25:06 +0800

    add monitoring in door area

--- scenes/game/act_2/scene_2/act_2_scene_2.gd
+++ scenes/game/act_2/scene_2/act_2_scene_2.gd
@@ -155,8 +155,10 @@ func switch_location(scene: PackedScene) -> void:
 
 	# === Connect Door Area Signals ===
 	if door_area:
+		door_area.monitoring = false 
 		door_area.body_entered.connect(_on_door_area_body_entered)
 		door_area.body_exited.connect(_on_door_area_body_exited)
+
 
 # ===================
 # CABINET AREA HANDLERS
@@ -283,6 +285,9 @@ func _on_chat_message_sent(chat_name: String) -> void:
 		Hud.clear_objectives()
 		ObjectiveManager.add_objective(scene_objectives[3]["ID"], scene_objectives[3]["text"])
 		DialogueManager.show_dialogue_balloon(A_2S_2, "go_outside")
+		
+		if door_area:
+			door_area.monitoring = true
 
 # ===================
 # COMPLETE SCENE
