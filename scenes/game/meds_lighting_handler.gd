0c9dea59c7ece4d31fd83be3fc4b5a749cadf6c6
Gerald <cancino.gerald138@gmail.com>
2025-10-24 13:29:46 +0800

    update meds lighting handler

--- scenes/game/meds_lighting_handler.gd
+++ scenes/game/meds_lighting_handler.gd
@@ -3,12 +3,19 @@ extends Node2D
 @onready var good_light = $good_lighting
 @onready var bad_light = $bad_lighting
 
+func _ready():
+	update_lighting_based_on_meds()
+
 func update_lighting_based_on_meds():
-	var meds = SaveManager.get_count_meds_taken()
+	var meds_done_count := SaveManager.get_count_meds_taken()
+	print("[MedsLighting] Meds taken count:", meds_done_count)
 	
-	if meds >= 2:
+	# Lighting logic: 1 = BAD, 2-3 = GOOD
+	if meds_done_count >= 2:
 		good_light.visible = true
 		bad_light.visible = false
+		print("[MedsLighting] Lighting: GOOD")
 	else:
 		good_light.visible = false
 		bad_light.visible = true
+		print("[MedsLighting] Lighting: BAD")
