8bfd9579ab680fa09e09c367f8878f33031ca00c
Gerald <cancino.gerald138@gmail.com>
2025-10-24 22:24:48 +0800

    add flicker if bad lighting

--- scenes/game/meds_lighting_handler.gd
+++ scenes/game/meds_lighting_handler.gd
@@ -3,14 +3,32 @@ extends Node2D
 @onready var good_light = $good_lighting
 @onready var bad_light = $bad_lighting
 
+var flicker_timer := 0.0
+var flicker_speed := 2.0
+var flicker_min := 0.99
+var flicker_max := 1.0
+
 func _ready():
 	update_lighting_based_on_meds()
+
+func _process(delta):
+	var meds_done_count := SaveManager.get_count_meds_taken()
+	if meds_done_count < 2:
+		bad_light.visible = true
+		good_light.visible = false
+		
+		flicker_timer += delta * flicker_speed
+		bad_light.energy = lerp(flicker_min, flicker_max, (sin(flicker_timer) + 1.0) / 2.0)
+	else:
+		bad_light.visible = false
+		good_light.visible = true
+		bad_light.energy = flicker_max
+
 
 func update_lighting_based_on_meds():
 	var meds_done_count := SaveManager.get_count_meds_taken()
 	print("[MedsLighting] Meds taken count:", meds_done_count)
 	
-	# Lighting logic: 1 = BAD, 2-3 = GOOD
 	if meds_done_count >= 2:
 		good_light.visible = true
 		bad_light.visible = false
