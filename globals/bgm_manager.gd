ad204e9f953c7db684f113bb872fb9752616d3df
Gerald <cancino.gerald138@gmail.com>
2025-10-31 23:11:27 +0800

    hardcode rain music in intro, title scene and a1s1

--- globals/bgm_manager.gd
+++ globals/bgm_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var bgm: AudioStreamPlayer = AudioStreamPlayer.new()
 const DEFAULT_MUSIC_PATH := "res://sound/soft-calm-piano-music-405074.mp3"
+const RAIN_BGM_PATH := "res://sound/calming-rain-257596.mp3"
 
 const MENU_SCENE_PATHS := [
 	"res://scenes/menu/boot_warning.tscn",
@@ -28,6 +29,18 @@ const MENU_SCENE_NAMES := [
 	"menu_report_a_bug"
 ]
 
+const RAIN_SCENE_PATHS := [
+	"res://scenes/game/intro_scene.tscn",
+	"res://scenes/game/act_1_title_scene.tscn",
+	"res://scenes/game/act_1/scene_1/act_1_scene_1.tscn"
+]
+
+const RAIN_SCENES := [
+	"intro_scene",
+	"act_1_title_scene",
+	"act_1_scene_1"
+]
+
 func _ready() -> void:
 	if not bgm.get_parent():
 		add_child(bgm)
@@ -48,11 +61,29 @@ func _check_current_scene() -> void:
 	var current_scene := get_tree().current_scene
 	if not current_scene:
 		return
-	if current_scene.name in MENU_SCENE_NAMES:
+
+	var scene_name := current_scene.name  # use the node name instead of filename
+
+	# --- RAIN BGM for game scenes ---
+	if scene_name in RAIN_SCENES:
+		if bgm.stream != preload(RAIN_BGM_PATH):
+			bgm.stream = preload(RAIN_BGM_PATH)
+			bgm.bus = "music"
+			bgm.volume_db = -20
+			if bgm.stream.has_method("set_loop"):
+				bgm.stream.set_loop(true)
+		if not bgm.playing:
+			bgm.play()
+		return  # skip normal menu checks
+
+	# --- Original menu music logic ---
+	if scene_name in MENU_SCENE_NAMES:
 		if not bgm.playing:
 			bgm.play()
 	else:
 		bgm.stop()
+
+
 
 func _on_scene_changed(_new_scene: Node) -> void:
 	_check_current_scene()
