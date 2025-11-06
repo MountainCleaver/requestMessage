fa3d4889b58e12013adb131430d3587bae45eaf1
clavvy <127577860+clavvy@users.noreply.github.com>
2025-11-07 02:51:32 +0800

    update bgm

--- globals/bgm_manager.gd
+++ globals/bgm_manager.gd
@@ -16,7 +16,11 @@ const MENU_SCENE_PATHS := [
 	"res://scenes/menu/menu_new_game_slots.tscn",
 	"res://scenes/menu/menu_load_game_slots.tscn",
 	"res://scenes/menu/privacy_policy.tscn",
-	"res://scenes/menu/menu_main.tscn"  # Added main menu
+	"res://scenes/menu/menu_main.tscn" , # Added main menu
+	"res://scenes/menu/menu_reset_password.tscn"
+	
+	
+	
 ]
 
 const RAIN_SCENE_PATHS := [
