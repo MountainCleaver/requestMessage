46a78ab0de9a30ce70bd1ca1ab90b8f53dac7586
clavvy <127577860+clavvy@users.noreply.github.com>
2025-11-22 00:37:39 +0800

    change #2

--- scenes/menu/scripts/menu_game_settings.gd
+++ scenes/menu/scripts/menu_game_settings.gd
@@ -1,12 +1,13 @@
 extends Control
 
-@onready var v_box_container: VBoxContainer = $options_label/VBoxContainer
+@onready var v_box_container: VBoxContainer = $VBoxContainer
 @onready var setting_container: Panel = $setting_container
 
-@onready var option_audio: Button = $options_label/VBoxContainer/option_audio
-@onready var option_display: Button = $options_label/VBoxContainer/option_display
-@onready var option_controls: Button = $options_label/VBoxContainer/option_controls
+@onready var option_audio: Button = $VBoxContainer/option_audio
+@onready var option_display: Button = $VBoxContainer/option_display
+@onready var option_controls: Button = $VBoxContainer/option_language
 
+@onready var back: Button = $back_tips/back
 @onready var exit: Button = $exit
 
 # Called when the node enters the scene tree for the first time.
@@ -17,11 +18,11 @@ func _ready() -> void:
 	var audio_settings := load("res://scenes/menu/settings_audio.tscn");
 	var audio_settings_instance = audio_settings.instantiate();
 	setting_container.add_child(audio_settings_instance);
+	back.pressed.connect(_on_back_pressed)
 
 #func _input(event: InputEvent) -> void:
 	#if event.is_action_pressed("escape"):
 		#_on_exit_pressed();
-
 
 func _on_option_audio_pressed() -> void:
 	_setting_switcher("res://scenes/menu/settings_audio.tscn")
@@ -54,3 +55,6 @@ func on_internet_status_changed(has_internet: bool) -> void:
 		pass
 	else:
 		print("No internet here, show warning or disable buttons.")
+
+func _on_back_pressed():
+	SignalBus.next_scene.emit("res://scenes/menu/menu_main.tscn")
